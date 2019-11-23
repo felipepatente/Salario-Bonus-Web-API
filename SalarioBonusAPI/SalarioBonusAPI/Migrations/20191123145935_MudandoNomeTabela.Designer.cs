b4dd2e833ef6b92bfb5eada613ea16dfa38ec4c8
felipepatente <felipepatente@yahoo.com.br>
2019-11-23 12:06:08 -0300

    Fazendo a alteração no banco para mudar o nome da tabela

--- SalarioBonusAPI/SalarioBonusAPI/Migrations/20191123145935_MudandoNomeTabela.Designer.cs
+++ SalarioBonusAPI/SalarioBonusAPI/Migrations/20191123145935_MudandoNomeTabela.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalarioBonusAPI.Data;
 
 namespace SalarioBonusAPI.Migrations
 {
     [DbContext(typeof(SalarioContext))]
-    partial class SalarioContextModelSnapshot : ModelSnapshot
+    [Migration("20191123145935_MudandoNomeTabela")]
+    partial class MudandoNomeTabela
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SalarioBonusAPI/SalarioBonusAPI/Migrations/20191123145935_MudandoNomeTabela.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalarioBonusAPI.Migrations
+{
+    public partial class MudandoNomeTabela : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SalarioBonusVendedor");
+
+            migrationBuilder.CreateTable(
+                name: "SalarioVendedor",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Vendedor = table.Column<string>(nullable: true),
+                    SalarioFixo = table.Column<decimal>(type: "Decimal(10,5)", nullable: false),
+                    TotalVendas = table.Column<decimal>(type: "Decimal(10,5)", nullable: false),
+                    SalarioReceber = table.Column<decimal>(type: "Decimal(10,5)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalarioVendedor", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SalarioVendedor");
+
+            migrationBuilder.CreateTable(
+                name: "SalarioBonusVendedor",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    SalarioFixo = table.Column<decimal>(type: "Decimal(10,5)", nullable: false),
+                    SalarioReceber = table.Column<decimal>(type: "Decimal(10,5)", nullable: false),
+                    TotalVendas = table.Column<decimal>(type: "Decimal(10,5)", nullable: false),
+                    Vendedor = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalarioBonusVendedor", x => x.ID);
+                });
+        }
+    }
+}
