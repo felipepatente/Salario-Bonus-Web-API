4d4dcefc7055521f5d7e045c7d94525dc640582f
felipepatente <felipepatente@yahoo.com.br>
2019-11-23 12:04:18 -0300

    Fazendo o CRUD no GerenciarSalario e fazendo o calculo do problema proposto no CalcularSalario

---
+++ SalarioBonusAPI/SalarioBonusAPI/Servicos/CalcularSalario.cs
@@ -0,0 +1,18 @@
+﻿using SalarioBonusAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalarioBonusAPI.Servicos
+{
+    public class CalcularSalario
+    {
+        private decimal comissao = 0.15m;
+
+        public decimal GetSalarioLiquido(SalarioVendedor salario)
+        {
+            return (salario.TotalVendas * comissao) + salario.SalarioFixo;
+        }
+    }
+}
---
+++ SalarioBonusAPI/SalarioBonusAPI/Servicos/GerenciarSalario.cs
@@ -0,0 +1,61 @@
+﻿using SalarioBonusAPI.Data;
+using SalarioBonusAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalarioBonusAPI.Servicos
+{
+    public class GerenciarSalario
+    {
+        private SalarioContext context;
+
+        public GerenciarSalario(SalarioContext context)
+        {
+            this.context = context;            
+        }
+
+
+        public SalarioVendedor AdicionarSalario(SalarioVendedor salario)
+        {
+            CalcularSalario calcular = new CalcularSalario();
+            salario.SalarioReceber = calcular.GetSalarioLiquido(salario);
+
+            context.SalarioVendedor.Add(salario);
+            context.SaveChanges();
+
+            return salario;
+        }
+        
+        public IList<SalarioVendedor> GetSalarioVendedores()
+        {
+            return context.SalarioVendedor.ToList();
+        }
+
+        public IList<SalarioVendedor> RemoverSalario(int id)
+        {
+            var salario = context.SalarioVendedor.SingleOrDefault(s => s.ID == id);
+
+            context.SalarioVendedor.Remove(salario);
+            context.SaveChanges();
+
+            return context.SalarioVendedor.ToList();
+
+        }
+
+        public SalarioVendedor AtualizarSalarioLiquido(SalarioVendedor salarioLiquidoNew, int id)
+        {
+            CalcularSalario calcularSalario = new CalcularSalario();
+
+            salarioLiquidoNew.ID = id;
+            salarioLiquidoNew.SalarioReceber = calcularSalario.GetSalarioLiquido(salarioLiquidoNew);
+
+            context.SalarioVendedor.Update(salarioLiquidoNew);
+            context.SaveChanges();
+
+
+            return context.SalarioVendedor.SingleOrDefault(s => s.ID == id);
+        }
+    }
+}
